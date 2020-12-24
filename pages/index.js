339d139585e2280ebb81c3c3a1295947443f9617
Matt Nicolls <2540582+nicolls1@users.noreply.github.com>
2020-12-24 11:52:00 +0100

    short description

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={'headingMd'}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, I'm testing out vercel :)</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
